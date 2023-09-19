3f364210a8b80829100bb29b0aceed45b7cfe01c
emnuelle <rm97973@fiap.com.br>
2023-09-19 11:39:50 -0300

    listagem dinâmica dos produtos

--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,26 @@
-import React, { Children } from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-// Blocos de rotas
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './routes/Home.jsx'
-import Produtos from './routes/Produtos.jsx'
-import EditarProdutos from './routes/EditarProdutos.jsx'
-import Erro404 from './routes/Erro404.jsx'
+//BLOCO DE ROTAS
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Home from './routes/Home.jsx';
+import Produtos from './routes/Produtos.jsx';
+import EditarProdutos from './routes/EditarProdutos.jsx';
+import Erro404 from './routes/Erro404.jsx';
 
-const router = createBrowserRouter([
-  {path: "/", element: <App/> , 
-  children:[
-    {path: "/", element: <Home/>},
-    {path: "/", element: <Produtos/>},
-    {path: "/", element: <EditarProdutos/>},
-    {path: "/", element: <Erro404/>},
-
-  ]
-  } 
+const router = createBrowserRouter([ 
+  {path: "/", element: <App/> , errorElement: <Erro404/>, 
+    children:[
+      {path: "/", element: <Home/>},
+      {path: "/produtos", element: <Produtos/>},
+      {path: "/editar/produtos/:id", element: <EditarProdutos/>},
+    ]
+  }
 ]);
 
+//BLOCO DE ROTAS
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    < RouterProvider router={router}/>
-  </React.StrictMode>,
+    <RouterProvider router={router}/>
 )
-
-
