d5cafe38f826513f25b2c3340bcf6c2954e100a6
emnuelle <rm97973@fiap.com.br>
2023-09-19 11:15:57 -0300

    Exemplos de hook, useEffectx3 + useState

---
+++ src/components/ListaProdutosExterna.jsx
@@ -0,0 +1,7 @@
+export const ListaProdutosExterna = [
+    {id:1, nome:"Teclado",desc:"Teclado 102 teclas mecânico",preoço:180},
+    {id:2, nome:"Monitor",desc:"32' 75Hz Tela Curva",preoço:2550},
+    {id:3, nome:"Mouse",desc:"Mouse óptico 6 botões",preoço:220},
+    {id:4, nome:"Mouse Pad",desc:"Mouse Pad Gamer 80x10",preoço:100},
+    {id:3, nome:"Head set",desc:"Head Set Gamer 8D",preoço:800},
+];
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
+
+  // Estrutura de declaração do useState
+  const[counter,setCounter] = useState(0);
+
+  document.title = "Lista de Produtos"
+
+  //Estrutura de declaração do useEffect que sempre executa.
+  useEffect(()=>{
+    console.log("Este useEffet renderiza sempre que houver uma atualização neste componente ou em um elemento filho");
+  });
+
+  //Estrutura de declaração do useEffect que executa uma única vez
+  useEffect(()=>{
+    console.log("Este useEffect renderiza renderiza apenas uma vez, no carregamento do componente!");
+  },[]);
+
+  //Estrutura de declaração do useState.
+  const[counter2,setCounter2] = useState(0);
+
+  //Estrutura de declaração do useEffect que executa sempre baseado em um determinado elemento. Este elemento pode ser:
+        //Uma constante, um componente, um objeto e ou uma variável. Que deve ser monitorados no array de dependências. [ ]
+        useEffect(()=>{
+          console.log("Este useEffect renderiza renderiza apenas quando o objeto monitorado sofre atualização.");
+        },[counter2]);
+
+  // const handleUseState = ()=>{
+  //   setCounter(1);
+  // };
+
   return (
-    <div>Home</div>
-  )
+    <div>
+      <h1>Lista de produtos</h1>
+      <div>
+        <button onClick={()=> setCounter(counter + 1)}>COUNTER - {counter}</button>
+      </div>
+      <div>
+        <button onClick={()=> setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
+      </div>
+    </div>
+  );
 }
