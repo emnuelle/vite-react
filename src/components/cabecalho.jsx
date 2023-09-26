a5c7bffbf9ba6c031eaeb9b981fb9126eef99824
emnuelle <rm97973@fiap.com.br>
2023-09-26 11:29:13 -0300

    mise

--- src/components/cabecalho.jsx
+++ src/components/cabecalho.jsx
@@ -1,21 +1,24 @@
 import { Link } from "react-router-dom";
+import styles from "./Cabecalho.module.css";
 
 export default function Cabecalho() {
+
+  
   return (
     <>
-        <header>
+        <header className={styles.cabecalho}>
           <h1>Vite + React</h1>
+          
+          <img src="/img/product.png" alt="Mãos segurando caixas." /> 
 
           {/* Crie uma lista com 5 links para as nossas rotas:
           Obs: Utilize o componente Link do router-dom */}
 
           <nav>
             <ul>
-              <li><Link to="/">HOME</Link> </li>
+              <li><Link to="/">HOME</Link></li>
               <li><Link to="/produtos">PRODUTOS</Link> </li>
               <li><Link to="/editar/produtos/1">Produto - 1</Link> </li>
-              <li><Link to="/editar/produtos/2">Produto - 2</Link> </li>
-              <li><Link to="/editar/produtos/3">Produto - 3</Link> </li>
             </ul>
           </nav>
 
@@ -23,3 +26,4 @@ export default function Cabecalho() {
     </>
   )
 }
+
--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { ListaProdutosExterna } from "../components/ListaProdutosExterna";
+import { Link } from "react-router-dom";
+import styles from "./Produtos.module.css";
 
 export default function Produtos() {
 
@@ -16,38 +18,38 @@ export default function Produtos() {
     <div>
       <h1>Lista de Produtos</h1>
 
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>NOME</th>
-                <th>DESCRÇÃO</th>
-                <th>PREÇO</th>
-                <th>EDITAR</th>
-              </tr>
-            </thead>
-            <tbody>
-            {
-              listaProdutosLocal.map((item,indice)=>(
-                <tr key={indice}>
-                  <td>{item.id}</td>
-                  <td>{item.nome}</td>
-                  <td>{item.descrição}</td>
-                  <td>{item.preco}</td>
-                  <td> <Link to={`/editar/produtos/${item.id}`}>Editar</Link> </td>
+          <div>
+            <table className={styles.tblEstilo}>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>NOME</th>
+                  <th>DESCRIÇÃO</th>
+                  <th>PREÇO</th>
+                  <th>EDITAR</th>
                 </tr>
-              ))
-            }
-            </tbody>
-            <tfoot>
-              <tr>
-                <td colSpan={4}>Prdutos Informáticos - QTD =</td>
-              </tr>
-            </tfoot>
-          </table>
-        </div>
-      
+              </thead>
+              <tbody>
+                {
+                  listaProdutosLocal.map((item,indice)=>(
+                    <tr key={indice}>
+                      <td>{item.id}</td>
+                      <td>{item.nome}</td>
+                      <td>{item.desc}</td>
+                      <td>{item.preco}</td>
+                      <td> <Link to={`/editar/produtos/${item.id}`}>Editar</Link> </td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td colSpan={5}>PRODUTOS INFORMÁTICOS - QTD = </td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+
     </div>
   )
 }
